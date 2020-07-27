d0d1617710cda05c63d3d4207450ab1ead3f1ae4
Moppit <michelletranusa@gmail.com>
2020-07-26 23:41:44 -0600

    past 2 weeks of work

---
+++ W9-DynamicProgramming/WarmUp/decode.py
@@ -0,0 +1,34 @@
+"""
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+NEED TO DO THIS ONE
+Source: https://leetcode.com/problems/decode-ways/discuss/30352/Accpeted-Python-DP-solution
+"""
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s == "": return 0
+        dp = [0 for x in range(len(s)+1)]
+        dp[0] = 1
+        for i in range(1, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if i != 1 and s[i-2:i] < "27" and s[i-2:i] > "09":  #"01"ways = 0
+                dp[i] += dp[i-2]
+        return dp[len(s)]
