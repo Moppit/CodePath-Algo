f713d1a6268a8cffed7fa99c2f66e0310081326d
Michelle Tran <michelletranusa@gmail.com>
2020-08-15 11:13:44 -0600

    Finished trie class and added test cases

--- W12-RecapAndDesign/trie.py
+++ W12-RecapAndDesign/trie.py
@@ -52,7 +52,6 @@ class Trie:
         """
         queue = [self.root, None]
         while queue:
-            
             curr_val = queue.pop(0)
             if curr_val == None:
                 print(' | ')
@@ -92,13 +91,30 @@ class Trie:
         """
         Returns if the word is in the trie.
         """
+        word_list = list(word)
+        word_list.append('nil')
+        curr_node = self.root
+        for char in word_list:
+            curr_set = curr_node.get_children()
+            if char in curr_set:
+                curr_node = curr_set[char]
+            else:
+                return False
+        return True
         
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        
+        curr_node = self.root
+        for char in prefix:
+            curr_set = curr_node.get_children()
+            if char in curr_set:
+                curr_node = curr_set[char]
+            else:
+                return False
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
@@ -108,9 +124,27 @@ class Trie:
 # param_3 = obj.startsWith(prefix)
 if __name__ == '__main__':
     trie = Trie()
-    trie.insert('hi')
+    inserting = ['hi', 'hello', 'hey']
+    print('Inserting:', inserting)
+    for word in inserting:
+        trie.insert(word)
     trie.print_trie()
-    trie.insert('hello')
-    trie.print_trie()
-    trie.insert('hey')
-    trie.print_trie()
+    
+    # Testing Search
+    print('---------------- TESTING SEARCH ----------------')
+    print('Search for "hi"', trie.search('hi'))
+    print('Search for "hello"', trie.search('hello'))
+    print('Search for "hey"', trie.search('hey'))
+    print('Search for "h"', trie.search('h'))
+    print('Search for "he"', trie.search('he'))
+    print('Search for ""', trie.search(''))
+    print('Search for "entirely different"', trie.search('entirely different'))
+    
+    # Testing Prefix
+    print('---------------- TESTING PREFIX ----------------')
+    print('Check prefix for "hi"', trie.startsWith('hi'))
+    print('Check prefix for "hello"', trie.startsWith('hello'))
+    print('Check prefix for "hey"', trie.startsWith('hey'))
+    print('Check prefix for "h"', trie.startsWith('h'))
+    print('Check prefix for ""', trie.startsWith(''))
+    print('Check prefix for "entirely different"', trie.startsWith('entirely different'))
